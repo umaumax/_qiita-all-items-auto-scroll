952b6d35ea4b9dc84d408bd158806aee57d61af9
umaumax <umauma7083@gmail.com>
2017-11-25 21:40:33 +0900

    Add various design

--- script.js
+++ script.js
@@ -1,27 +1,47 @@
 // NOTE: to avoid multi click
 var scrollFlag = false;
-$(window).bind("scroll", function() {
-	let windowHeight = $(window).height();
-	let scrollHeight = $(document).height();
-	let scrollPosition = $(window).height() + $(window).scrollTop();
-	// hide tag ranking
-	$('.ra-TagList').hide();
-	// charnge main content width
-	$('.p-home_main').css('width', '80%');
-	// remove color inherit (to detect visited link color)
-	$('a').css('color', '-webkit-link');
+
+// for https://qiita.com/*/items/*
+$(function(){
+	// hide comment form
+	$('.p-items_container').eq(1).hide();
+	// hide footer
+	$('.footer').hide();
 	// extend content
 	$('.p-items_container').css('max-width', 'initial');
 	$('.p-items_container').css('grid-template-columns', '80px calc(100% - 200px - 80px) 200px');
 	$('.p-items_toc').css('width', '100%');
+	// cahnge color
+	$('.p-items_article').css('background-color', '#ddf');
+	$('.p-items_wrapper').css('background-color', '#ccf');
+	$('.it-Header_time').css('color', '#000');
+
+	// FYI: [【jQuery】動的にlinkタグとstyleタグを追加する \- Tsukikuro Blog](http://tsukikuro.hatenablog.com/entry/2013/12/10/192250)
+		// for https://qiita.com/items
+		// hide tag ranking
+		// charnge main content width
+		// remove color inherit (to detect visited link color)
+		// NOTE: ':visited'はセキュリティの関係上jsからは操作できないとのこと
+		var style = '<style type="text/css">'+
+			'.ra-TagList { display: none; }'+
+			'.p-home_main { width:80%; }'+
+			'a.tf-ItemContent_title { color:#337ab7 !important; }'+
+			'a:visited.tf-ItemContent_title { color:#685987 !important; }'+
+			'</style>';
+		$(style).appendTo('head');
+});
+
+// for https://qiita.com/items
+$(window).bind("scroll", function() {
+	let windowHeight = $(window).height();
+	let scrollHeight = $(document).height();
+	let scrollPosition = $(window).height() + $(window).scrollTop();
 	if ( (scrollHeight - scrollPosition) / windowHeight <= 0.10) {
 		if (!scrollFlag) {
 			scrollFlag=true;
 			// スクロールの位置が下からn%
 			// auto scroll
 			let elem = $('.af-ItemList_moreButton');
-			console.log('hello');
-			console.log(elem);
 			$('.af-ItemList_moreButton').click();
 		}
 	} else {
